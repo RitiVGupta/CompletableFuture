a1649aa7044106e8bb9df06502fd2fcb88998255
RitiVGupta <ritivgupta@gmail.com>
2026-01-01 22:47:35 +0530

    Commit on my branch only

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ void main() {
 
     CompletableFuture<String> futureFirstName =  CompletableFuture.supplyAsync(this::getFirstName);
     CompletableFuture<String> futureLastName =  CompletableFuture.supplyAsync(this::getLastName);
-    CompletableFuture<String> futureResult = futureFirstName.thenCombine(futureLastName,(f1,f2) -> f1 + " " + f2);
+    CompletableFuture<String> futureMiddleName =  CompletableFuture.supplyAsync(this::getMiddleName);
+    CompletableFuture<String> futureResult = futureFirstName.
+            thenCombine(futureMiddleName,(f1,f2) -> f1 + " " + f2).
+            thenCombine(futureLastName,(f1,f2) -> f1 + " " + f2);
     try {
         System.out.println(futureResult.get());
     } catch (InterruptedException | ExecutionException e) {
@@ -22,5 +25,11 @@ private String getFirstName()
 private String getLastName()
 {
     return "Future";
+
+}
+
+private String getMiddleName()
+{
+    return "For";
 
 }
